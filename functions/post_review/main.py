cf93646112735aa9afafea6954f898345a6a0504
optionsreaper <tim@schreckengineering.com>
2022-06-21 04:00:23 +0000

    post_review updates

--- functions/post_review/main.py
+++ functions/post_review/main.py
@@ -7,7 +7,7 @@
 # @return The output of this action, which must be a JSON object.
 #
 #
-from ibmcloudant.cloudant_v1 import CloudantV1
+from ibmcloudant.cloudant_v1 import CloudantV1, Document
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_cloud_sdk_core import ApiException
 import requests
@@ -21,25 +21,33 @@ def main(dict):
         client = CloudantV1(authenticator=authenticator)
         client.set_service_url(dict['COUCH_URL'])
         
+        new_review = Document(
+            id=str(dict['review']['id']),
+            name=dict['review']['name'],
+            dealership=dict['review']['dealership'],
+            review=dict['review']['review'],
+            purchase=dict['review']['purchase'],
+            another=dict['review']['another'],
+            purchase_date=dict['review']['purchase_date'],
+            car_make=dict['review']['car_make'],
+            car_model=dict['review']['car_model'],
+            car_year=dict['review']['car_year']
+        )
 
-        if "dealerId" in dict:
-            response = client.post_find(
-                db=databaseName,
-                selector={"dealership":int(dict["dealerId"])}
-            ).get_result()["docs"]
-
-            if len(response) != 0:
-                return {
-                    "statusCode":200,
-                    "headers":{ 'Content-Type': 'application/json'},
-                    "body": response
-                }
+        response = client.post_document(db='reviews', document=new_review).get_result()
 
         return {
-            "statusCode":404,
-            "headers":{ 'Content-Type': 'text/plain'},
-            "body": "dealerId does not exist"
+            "statusCode":200,
+            "headers":{ 'Content-Type': 'application/json'},
+            "body": "Successfully added"
         }
+
+    except ApiException as ae:
+        print("Method failed")
+        print(" - status code: " + str(ae.code))
+        print(" - error message: " + ae.message)
+        if ("reason" in ae.http_response.json()):
+            print(" - reason: " + ae.http_response.json()["reason"])
 
     except:
         return {
