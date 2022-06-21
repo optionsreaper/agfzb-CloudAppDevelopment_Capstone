1a297b1f714cbdd2cc1a59af62a3de62f1793084
optionsreaper <tim@schreckengineering.com>
2022-06-21 04:20:28 +0000

    added car make and car model Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name: models.TextField(null=False)
+    description: models.TextField(null=False)
+
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+               "Description: " + self.description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +26,26 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = "sedan"
+    SUV = "suv"
+    WAGON = "wagon"
+    MODEL_TYPES = [
+        (SEDAN, "Sedan"),
+        (SUV, "SUV"),
+        (WAGON, "Wagon")
+    ]
+    car_make: models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name: models.TextField(null=False)
+    dealer_id: models.IntegerField(null=False)
+    type: models.CharField(null=False, choices=MODEL_TYPES, default=SEDAN, max_length=20)
+    year: models.DateField(null=False)
+
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+               "Dealer ID: " + self.dealer_id + ", " + \
+                "Type: " + self.type + ", " + \
+                "Year: " + self.year
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
